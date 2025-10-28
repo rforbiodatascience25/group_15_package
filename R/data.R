b8efc5dbe13486b9bea5673433818e6592c09341
Anasy98 <anaselyoussef@gmail.com>
2025-10-28 09:48:08 +0100

    description added

--- R/data.R
+++ R/data.R
@@ -1,7 +1,7 @@
 #' Title
-#' 
+#'
 #' Description
-#' 
-#' 
+#' First codon table for the package
+#'
 #' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
 "codon_table"
