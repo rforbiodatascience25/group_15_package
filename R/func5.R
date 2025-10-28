64bf007323345fc5d1382016cad05069ecbcbde5
palssonjoel <s253731@student.dtu.dk>
2025-10-28 10:56:00 +0100

    added function

--- R/func5.R
+++ R/func5.R
@@ -1,14 +1,27 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   https://r-pkgs.org
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
+#' Title
+#'
+#' @param param1
+#' @param param2
+#' @importFrom stringr str_split boundary str_count
+#' @import ggplot2
+name_me1 <- function(name_me2){
+
+  name_me3 <- name_me2 |>
+    stringr::str_split(pattern = stringr::boundary("character"), simplify = TRUE) |>
+    as.character() |>
+    unique()
+
+  counts <- sapply(name_me3, function(amino_acid) stringr::str_count(string = name_me2, pattern =  amino_acid)) |>
+    as.data.frame()
+
+  colnames(counts) <- c("Counts")
+  counts[["Name_me2"]] <- rownames(counts)
+
+  name_me4 <- counts |>
+    ggplot2::ggplot(ggplot2::aes(x = Name_me2, y = Counts, fill = Name_me2)) +
+    ggplot2::geom_col() +
+    ggplot2::theme_bw() +
+    ggplot2::theme(legend.position = "none")
+
+  return(name_me4)
+}
