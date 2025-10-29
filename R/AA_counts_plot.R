6a10ba9564b16ff74aa4ed383bbef5bfc1dfd5a3
palssonjoel <s253731@student.dtu.dk>
2025-10-29 18:26:04 +0100

    Wrote vignette for function 5 and minor fixes of function variable names.

--- R/AA_counts_plot.R
+++ R/AA_counts_plot.R
@@ -3,8 +3,10 @@
 #' This function takes a string of one letter amino acid codes,
 #'  and generates a barplot showing the counts of each amino acid.
 #'
+#'  While designed for amino acids sequences, the function also works with DNA and RNA sequences.
 #'
-#' @param AA_string A string of one letter amino acid codes. Any non-amino acids will be ignored.
+#'
+#' @param sequence A string of one letter amino acid codes or nucleotides. Any non-amino acids or nucleotides will be ignored.
 #' @returns geom_col with amino acids on x-axis and counts on y.
 #'
 #' @importFrom stringr str_split boundary str_count
@@ -12,27 +14,27 @@
 #'
 #' @examples
 #' AA_counts_plot("SSFPTAYYHQNNQQ")
-AA_counts_plot <- function(AA_string){
+AA_counts_plot <- function(sequence){
 
   #Separate string into character vector by character
-  amino_acids <- AA_string |>
+  amino_acids <- sequence |>
     stringr::str_split(pattern = stringr::boundary("character"), simplify = TRUE) |>
     as.character() |>
     unique()
 
   #Count number of times each amino acid is found in the character vector amino_acids
-  counts <- sapply(amino_acids, function(amino_acid) stringr::str_count(string = AA_string, pattern =  amino_acid)) |>
+  counts <- sapply(amino_acids, function(amino_acid) stringr::str_count(string = sequence, pattern =  amino_acid)) |>
     as.data.frame()
 
   #Create list with amino acids and their counts
   colnames(counts) <- c("Counts")
-  counts[["AA_string"]] <- rownames(counts)
+  counts[["sequence"]] <- rownames(counts)
 
-  AA_barplot <- counts |>
-    ggplot2::ggplot(ggplot2::aes(x = AA_string, y = Counts, fill = AA_string)) +
+  sequence_barplot <- counts |>
+    ggplot2::ggplot(ggplot2::aes(x = sequence, y = Counts, fill = sequence)) +
     ggplot2::geom_col() +
     ggplot2::theme_bw() +
     ggplot2::theme(legend.position = "none")
 
-  return(AA_barplot)
+  return(sequence_barplot)
 }
